53323144a8c8775b81ff306dcecf7e53fd91c128
Felipe <felipeush@gmail.com>
2024-06-10 16:19:43 -0300

    onboarding form structure done

--- src/app/(auth)/onboarding/page.tsx
+++ src/app/(auth)/onboarding/page.tsx
@@ -1,5 +1,28 @@
+import AccountProfile from "@/components/forms/account-profile";
 import React from "react";
+import { currentUser } from "@clerk/nextjs/server";
 
-export default function OnboardingPage() {
-  return <main className="text-white">OnboardingPage</main>;
+export default async function OnboardingPage() {
+  const user = await currentUser();
+  const userInfo = {};
+  const userData = {
+    id: user?.id,
+    objectId: userInfo?._id,
+    username: userInfo?.username || user?.username,
+    name: userInfo?.name || user?.fullName,
+    bio: userInfo?.bio || "",
+    image: userInfo?.image || user?.imageUrl,
+  };
+
+  return (
+    <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
+      <h1 className="text-heading2-bold text-light-1">Onboarding</h1>
+      <p className="text-light-2 mt-2 text-base-regular">
+        Complete your profile now to use Threads
+      </p>
+      <section className="mt-9 bg-dark-2 p-10 rounded-xl">
+        <AccountProfile user={userData} btnTitle="Continue" />
+      </section>
+    </main>
+  );
 }
